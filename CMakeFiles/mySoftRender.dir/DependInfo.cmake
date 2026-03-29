8149a36a900ab5578ee2f02c1e96be747eca9c36
Yao <3087709478@qq.com//>
2026-03-29 17:15:34 +0800

    first push

---
+++ CMakeFiles/mySoftRender.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xiaoyao/Desktop/mySoftRender/src/main.cpp" "CMakeFiles/mySoftRender.dir/src/main.cpp.o" "gcc" "CMakeFiles/mySoftRender.dir/src/main.cpp.o.d"
+  "/home/xiaoyao/Desktop/mySoftRender/src/software_renderer.cpp" "CMakeFiles/mySoftRender.dir/src/software_renderer.cpp.o" "gcc" "CMakeFiles/mySoftRender.dir/src/software_renderer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/mySoftRender.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mySoftRender.
+# This may be replaced when dependencies are built.
